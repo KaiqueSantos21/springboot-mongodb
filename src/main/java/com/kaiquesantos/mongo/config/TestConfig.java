5d0618b6cb1ca4d9225c0f711621a20097d37a76
Kaique Santos <kaiqueosantos@gmail.com>
2022-06-10 14:50:28 -0300

    Projection of author data with DTO

--- src/main/java/com/kaiquesantos/mongo/config/TestConfig.java
+++ src/main/java/com/kaiquesantos/mongo/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.kaiquesantos.mongo.domain.Post;
 import com.kaiquesantos.mongo.domain.User;
+import com.kaiquesantos.mongo.dto.AuthorDTO;
 import com.kaiquesantos.mongo.repository.PostRepository;
 import com.kaiquesantos.mongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class TestConfig implements CommandLineRunner {
 		User alex = new User(null, "Alex", "alex@gmail.com");
 		User bob = new User(null, "Bob", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abracos!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abracos!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 }
