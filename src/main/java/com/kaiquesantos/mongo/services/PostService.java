3e383b9ec39a45fb6ce1a7d0bc69d24231a403ed
Kaique Santos <kaiqueosantos@gmail.com>
2022-06-10 15:09:52 -0300

    Finding post by id

---
+++ src/main/java/com/kaiquesantos/mongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.kaiquesantos.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaiquesantos.mongo.domain.Post;
+import com.kaiquesantos.mongo.repository.PostRepository;
+import com.kaiquesantos.mongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado"));
+	}
+}
